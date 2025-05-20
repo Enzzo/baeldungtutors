40e763351c5991abc5c681362b0be33295c18fee
unknown <enzzomatrix@gmail.com>
2025-05-20 11:50:04 +1100

    init

---
+++ 01 spring framework introduction/01 intro to building application/app/src/test/java/ru/vasilev/customai/CustomaiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasilev.customai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
