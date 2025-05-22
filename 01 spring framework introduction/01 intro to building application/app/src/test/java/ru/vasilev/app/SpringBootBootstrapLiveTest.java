62ff93c0cd711c657c5618afe391dce71c438e92
unknown <enzzomatrix@gmail.com>
2025-05-22 17:52:10 +1100

    upd

--- 01 spring framework introduction/01 intro to building application/app/src/test/java/ru/vasilev/app/SpringBootBootstrapLiveTest.java
+++ 01 spring framework introduction/01 intro to building application/app/src/test/java/ru/vasilev/app/SpringBootBootstrapLiveTest.java
@@ -30,8 +30,9 @@ public class SpringBootBootstrapLiveTest {
 	
 	private Book createRandomBook() {
 		final Book book = new Book();
-		book.setTitle("title1");
-		book.setAuthor("author1");
+		int x = (int)Math.random();
+		book.setTitle("title" + x);
+		book.setAuthor("author" + x);
 		return book;
 	}
 	
@@ -69,35 +70,61 @@ public class SpringBootBootstrapLiveTest {
 		assertEquals(book.getTitle(), response.jsonPath().get("title"));
 	}
 	
-	@Test
-	public void whenGetNotExistBookByIdThenNotFound() {
-		Response response = RestAssured.get(API_ROOT + "/" + 9);
-		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
-	}
-	
-	@Test
-	public void whenCreateNewBookThenCreated() {
-		Book book = createRandomBook();
-		Response response = RestAssured.given()
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.body(book)
-				.post(API_ROOT);
-		assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
-	}
-	
-	@Test
-	public void whenInvalidBookThenError() {
-		Book book = createRandomBook();
-		book.setAuthor(null);
-		Response response = RestAssured.given()
-				.contentType(MediaType.APPLICATION_JSON_VALUE)
-				.body(book)
-				.post(API_ROOT);
-		assertEquals(HttpStatus.BAD_REQUEST.value(),response.getStatusCode());
-	}
-	
-	@Test
-	public void whenUpdateCreatedBookThenUpdated() {
-		
-	}
+//	@Test
+//	public void whenGetNotExistBookByIdThenNotFound() {
+//		Response response = RestAssured.get(API_ROOT + "/" + 9);
+//		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
+//	}
+//	
+//	@Test
+//	public void whenCreateNewBookThenCreated() {
+//		Book book = createRandomBook();
+//		Response response = RestAssured.given()
+//				.contentType(MediaType.APPLICATION_JSON_VALUE)
+//				.body(book)
+//				.post(API_ROOT);
+//		assertEquals(HttpStatus.CREATED.value(), response.getStatusCode());
+//	}
+//	
+//	@Test
+//	public void whenInvalidBookThenError() {
+//		Book book = createRandomBook();
+//		book.setAuthor(null);
+//		Response response = RestAssured.given()
+//				.contentType(MediaType.APPLICATION_JSON_VALUE)
+//				.body(book)
+//				.post(API_ROOT);
+//		assertEquals(HttpStatus.BAD_REQUEST.value(),response.getStatusCode());
+//	}
+//	
+//	@Test
+//	public void whenUpdateCreatedBookThenUpdated() {
+//		Book book = createRandomBook();
+//		String location = createBookAsUri(book);
+//		book.setId(Long.parseLong(location.split("api/books/")[1]));
+//		book.setAuthor("newAuthor");
+//		Response response = RestAssured.given()
+//				.contentType(MediaType.APPLICATION_JSON_VALUE)
+//				.body(book)
+//				.put(location);
+//		
+//		assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+//		
+//		response = RestAssured.get(location);
+//		
+//		assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+//		assertEquals("newAuthor", response.jsonPath().get("author"));
+//	}
+//	
+//	@Test
+//	public void whenDeleteCreatedBookThenOk() {
+//		Book book = createRandomBook();
+//		String location = createBookAsUri(book);
+//		Response response = RestAssured.delete(location);
+//		
+//		assertEquals(HttpStatus.OK.value(), response.getStatusCode());
+//		
+//		response = RestAssured.get(location);
+//		assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatusCode());
+//	}
 }
